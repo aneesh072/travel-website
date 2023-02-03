aca0d144e644d8df3b3140805c95c1fdd749452b
Enis Karki <aneeshkarki292@gmail.com>
2023-02-03 19:41:41 +1100

    nav change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 const Hero = () => {
   return (
     <div className="hero" id="hero">
       <div className="hero-detail">
         <h1>Lets leave together to the wonderland</h1>
-        <button>Lets Talk</button>
+        <button>
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+          >
+            Lets Talk
+          </Link>
+        </button>
       </div>
     </div>
   );
